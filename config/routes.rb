41c7c9f93051724260c851fb02c6358c1e71d783
tpsingh <tpsingh@enbake.com>
2014-02-10 19:22:04 +0530

    Added Metronic to logout pages + alerts

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,11 @@ LoginApp::Application.routes.draw do
 
   devise_for :users, controllers: {sessions: "users/sessions", registrations: "users/registrations", passwords: "users/passwords"}
   devise_scope :user do
-    match 'login', to: "users/registrations#auth", via: [:get, :post]
+    match 'login', to: "users/sessions#new", via: [:get, :post], as: "login"
     get "logout", to: "devise/sessions#destroy", as: "logout"
     match '/add_mail' => 'users/registrations#add_mail', via: [:get, :post]
-    get '/auth/failure' => "users/registrations#auth"
-    get 'return' => "users/registrations#auth"
+    get '/auth/failure' => "users/sessions#new"
+    get 'return' => "users/sessions#new"
   end
   root 'welcome#index'
   match '/auth/:provider/callback' => 'authentication#create', via: [:get, :post]
