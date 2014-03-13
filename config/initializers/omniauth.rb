6cdf679fbb8cc9a75a5958455656bd4e5671e5f0
Ishu <ishwinder@enbake.com>
2014-03-14 00:41:07 +0530

    New Open Auth Keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "UCy4NZ9aItctPX635i7tA", "Z5JEHwGC3MbCGPSSXo5XBUVWvNY3J02lJIop1FChc"
-  provider :google_oauth2, "168268692145-ftej6o88omro37vst1lk3khvr4tk373i.apps.googleusercontent.com", "Q-OWLYtj4AyZXVeXuLTTSxm0"
+  provider :twitter, "opocukjvQIxgY3exHSGUCQ", "iivLsD3q5BsaM0GKM54cAyoi4tywEYn3dodKXDoN0"
+  provider :google_oauth2, "664022931868-6n8b2cm15ub1ic8a63m9bhdr6krcsh5e.apps.googleusercontent.com", "b9f52a44ed549452e64a74b0fbfb520e5a62ff16"
 
   OmniAuth.config.on_failure = Proc.new { |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
