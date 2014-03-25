a8bd28a653207f85ab2f52e5fdc4200e2dceaf1a
Ishu <ishwinder@enbake.com>
2014-03-25 12:55:44 +0530

    Fixed the Google key. Products helper for rendering product images.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
+  def product_image(product)
+    image_path = case product
+    when "vT6Basic"
+      "Vtiger-logo.png"
+    when "Sugar6516"
+      "logo_sugarcrm.png"
+    end
+    image_tag image_path, class: 'img-responsive', style: 'text-align:center'
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, "opocukjvQIxgY3exHSGUCQ", "iivLsD3q5BsaM0GKM54cAyoi4tywEYn3dodKXDoN0"
-  provider :google_oauth2, "664022931868-6n8b2cm15ub1ic8a63m9bhdr6krcsh5e.apps.googleusercontent.com", "b9f52a44ed549452e64a74b0fbfb520e5a62ff16"
+  provider :google_oauth2, "664022931868-48jsgcas73niirgr2bsg6pe0rllk63mu.apps.googleusercontent.com", "h4sYLgpzAI76-f0NQuzb1rwv"
 
   OmniAuth.config.on_failure = Proc.new { |env|
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
